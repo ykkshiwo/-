cadb2b2992868bd7c6df4978e77b4f3d752df95d
yangkangkai <452306233@qq.com>
2018-11-16 17:46:53 +0800

    添加教练信息

--- trains_info.js
+++ trains_info.js
@@ -4,36 +4,108 @@ var data_group_trains=
 			[
 			{
 				en_name: "Darren",
-				cn_name: "曾钟钟1",
+				cn_name: "曾钟钟",
+				position: "佑远团操总监",
+				img: "",
 				certificates: [
-					"2012年获得RYS200ttc证书",
-					"2013年获得RYS50小时流瑜伽证书",
-					"2016获得Yoga Beyond 学院飞行瑜伽100小时Level 1证书",
-					"2017获得Yoga Beyond 学院飞行瑜伽100小时Level 2证书",
+					"Schwinn单车一星认证",
+					"Train Fitness体系fight fx认证",
+					"莱美体系bodypump初始培训",
 				],
-				projects: ["尊巴","有氧","杠铃","单车","搏击"]
+				projects: ["LesMills BODYPUMP","LesMills BODYCOMBAT","LesMills CXWORD","Fight FX能量搏击","PUMP FX功能杠铃","Zumba","Adidas训练课","Schwinn十字星单车"]
 			},
 			{
-				en_name: "Darren",
-				cn_name: "曾钟钟2",
+				en_name: "Yiyi",
+				cn_name: "李玮秋",
+				position: "佑远团操教练",
+				img: "",
 				certificates: [
-					"2012年获得RYS200ttc证书",
-					"2013年获得RYS50小时流瑜伽证书",
-					"2016获得Yoga Beyond 学院飞行瑜伽100小时Level 1证书",
-					"2017获得Yoga Beyond 学院飞行瑜伽100小时Level 2证书",
+					"中国瑜伽师联盟高级瑜伽导师",
+					"阴瑜伽高伽导师认证",
+					"流瑜伽瑜伽导师认证",
+					"高温瑜伽瑜伽导师认证",
 				],
-				projects: ["尊巴","有氧","杠铃","单车","搏击"]
+				projects: ["哈他瑜伽","阴瑜伽","流瑜伽","理疗瑜伽","高温瑜伽"]
+			},
+			]
+	};
+
+var data_person_trains=
+	{
+		trains_info: 
+			[
+			{
+				en_name: "Echo",
+				cn_name: "张慧丽",
+				position: "佑远私人教练",
+				certificates: [
+					"国家职业资格证书",
+					"红十字会急救员证",
+					"功能性训练技能鉴定证书",
+					"普拉提（康复训练）技能鉴定证书",
+					"运动康复3M拉伸技能鉴定证书",
+					"拳课技能鉴定证书",
+				],
+				projects: ["拉伸","增肌","减脂","塑形"]
 			},
 			{
-				en_name: "Darren",
-				cn_name: "曾钟钟1",
+				en_name: "Rick",
+				cn_name: "陈伟",
+				position: "佑远私人教练",
 				certificates: [
-					"2012年获得RYS200ttc证书",
-					"2013年获得RYS50小时流瑜伽证书",
-					"2016获得Yoga Beyond 学院飞行瑜伽100小时Level 1证书",
-					"2017获得Yoga Beyond 学院飞行瑜伽100小时Level 2证书",
+					"CBBA高级私人教练认证",
+					"体适能认证",
 				],
-				projects: ["尊巴","有氧","杠铃","单车","搏击"]
-			}
+				projects: ["增肌","减脂","塑形"]
+			},
+			{
+				en_name: "Shiny Cheng",
+				cn_name: "程丰明",
+				position: "佑远私人教练",
+				certificates: [
+					"CBBA高级私人教练认证",
+					"MFT健身格斗认证",
+					"健康训练师认证 ",
+				],
+				projects: ["减脂","塑形","体能强化","格斗"]
+			},
+			{
+				en_name: "Javon",
+				cn_name: "贾鹏",
+				position: "佑远私人教练",
+				certificates: [
+					"国家职业资格认证",
+					"体态矫正认证",
+					"运动康复认证",
+					"运动训练师认证",
+					"体适能认证"
+				],
+				projects: ["拉伸","体态矫正","运动康复","增肌","减脂","塑形"]
+			},
+				{
+				en_name: "Alick Li",
+				cn_name: "李文华",
+				position: "佑远私人教练",
+				certificates: [
+					"国家职业资格认证",
+					"上海红十字会急救（CPR）认证",
+					"TRX认证",
+					"极致瘦身认证",
+				],
+				projects: ["增肌","减脂","塑形"]
+			},
+				{
+				en_name: "Page Ning",
+				cn_name: "宁康荘",
+				position: "佑远私人教练",
+				certificates: [
+					"国家职业资格认证",
+					"体态矫正认证",
+					"运动康复认证",
+					"运动训练师认证",
+					"体适能认证"
+				],
+				projects: ["拉伸","体态矫正","运动康复","增肌","减脂","塑形"]
+			},
 			]
 	};
