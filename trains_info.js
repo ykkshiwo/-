29bc2952db80e9123ee9cf2a2ef2a8f555629234
yangkangkai <452306233@qq.com>
2018-11-16 16:58:58 +0800

    教练数据输入

---
+++ trains_info.js
@@ -0,0 +1,39 @@
+var data_group_trains=
+	{
+		trains_info: 
+			[
+			{
+				en_name: "Darren",
+				cn_name: "曾钟钟1",
+				certificates: [
+					"2012年获得RYS200ttc证书",
+					"2013年获得RYS50小时流瑜伽证书",
+					"2016获得Yoga Beyond 学院飞行瑜伽100小时Level 1证书",
+					"2017获得Yoga Beyond 学院飞行瑜伽100小时Level 2证书",
+				],
+				projects: ["尊巴","有氧","杠铃","单车","搏击"]
+			},
+			{
+				en_name: "Darren",
+				cn_name: "曾钟钟2",
+				certificates: [
+					"2012年获得RYS200ttc证书",
+					"2013年获得RYS50小时流瑜伽证书",
+					"2016获得Yoga Beyond 学院飞行瑜伽100小时Level 1证书",
+					"2017获得Yoga Beyond 学院飞行瑜伽100小时Level 2证书",
+				],
+				projects: ["尊巴","有氧","杠铃","单车","搏击"]
+			},
+			{
+				en_name: "Darren",
+				cn_name: "曾钟钟1",
+				certificates: [
+					"2012年获得RYS200ttc证书",
+					"2013年获得RYS50小时流瑜伽证书",
+					"2016获得Yoga Beyond 学院飞行瑜伽100小时Level 1证书",
+					"2017获得Yoga Beyond 学院飞行瑜伽100小时Level 2证书",
+				],
+				projects: ["尊巴","有氧","杠铃","单车","搏击"]
+			}
+			]
+	};
