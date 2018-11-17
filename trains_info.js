52f33e5e3483f9cd915abd00e13ba46e9e874dc1
yangkangkai <452306233@qq.com>
2018-11-17 17:26:23 +0800

    教练信息录入完毕

--- trains_info.js
+++ trains_info.js
@@ -1,3 +1,4 @@
+//团操教练
 var data_group_trains=
 	{
 		trains_info: 
@@ -30,6 +31,7 @@ var data_group_trains=
 			]
 	};
 
+//私人教练
 var data_person_trains=
 	{
 		trains_info: 
@@ -99,13 +101,103 @@ var data_person_trains=
 				cn_name: "宁康荘",
 				position: "佑远私人教练",
 				certificates: [
-					"国家职业资格认证",
-					"体态矫正认证",
-					"运动康复认证",
-					"运动训练师认证",
-					"体适能认证"
+					"健身教练国家职业中级认证",
+					"TRX悬吊体适能教练证书",
+					"AASFP体适能教练认证",
+					"AASFP动感单车教练认证",
 				],
-				projects: ["拉伸","体态矫正","运动康复","增肌","减脂","塑形"]
+				projects: ["体态评估","增肌","减脂","塑形"]
+			},
+				{
+				en_name: "Fish",
+				cn_name: "孙路",
+				position: "佑远私人教练",
+				certificates: [
+					"国家职业认证",
+					"上海红十字会急救（CPR）认证",
+					"TRX认证",
+					"Honour极致瘦身认证",
+				],
+				projects: ["增肌","减脂","塑形"]
+			},
+				{
+				en_name: "Jack wang",
+				cn_name: "王斌",
+				position: "佑远私人教练",
+				certificates: [
+					"健身教练国家职业高级认证",
+					"TRX悬吊体适能教练证书",
+					"AASFP体适能教练认证",
+					"AASFP动感单车教练认证",
+					"2015年温州市全国邀请赛男子健美75KG第四名"
+				],
+				projects: ["增肌","急速减脂","塑形","肌肉放松","拉伸","体态矫正","健美指导"]
+			},
+				{
+				en_name: "Rambo",
+				cn_name: "王刚",
+				position: "佑远私人教练",
+				certificates: [
+					"高级国家职业资格健身教练",
+					"中国跆拳道协会黑带认证",
+					"南昌市跆拳道协会普通晋段考试官",
+					"深圳市力仕健身学院（团体课）金牌培训导师",
+					"Trend UBound课程培训导师",
+					"Balas--潮流拳击认证教练"
+				],
+				projects: ["增肌","减脂","塑形","拳击","拉伸","体态矫正","散打","跆拳道"]
+			},
+				{
+				en_name: "Flute Wang",
+				cn_name: "王天将",
+				position: "佑远私人教练",
+				certificates: [
+					"CPR应急救护",
+					"CPT私人教练认证",
+					"体适能认证",
+					"运动训练师认证",
+					"体态评估与矫正",
+					"运动复健师认证",
+					"国家体育总局健身教练职业资格认证"
+				],
+				projects: ["减脂","塑形","拉伸"]
+			},
+				{
+				en_name: "Alex",
+				cn_name: "徐金桥",
+				position: "佑远私人教练",
+				certificates: [
+					"赛普体适能认证",
+					"AFCC高级私人教练认证",
+					"CBBA中国健美协会高级私人教练",
+					"SSC体重管理认证教练",
+					"提睾扶阳专业认证",
+				],
+				projects: ["减脂","塑形","拉伸","增肌","康复","拳击"]
+			},
+				{
+				en_name: "JeanZhang",
+				cn_name: "张熙昊",
+				position: "佑远私人教练",
+				certificates: [
+					"LFBb国际私人教练认证",
+					"产后康复认证",
+					"中国红十字会认证 ",
+				],
+				projects: ["减脂","塑形","增肌"]
+			},
+				{
+				en_name: "Allen",
+				cn_name: "赵军",
+				position: "佑远私人教练",
+				certificates: [
+					"国家职业资格认证",
+					"健康体适能认证",
+					"体态矫正师认证",
+					"运动训练师认证",
+					"运动康复师认证"
+				],
+				projects: ["减脂","塑形","增肌"]
 			},
 			]
 	};
