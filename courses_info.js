83b62e5848e952a509584dd46a606bd1b12bb14e
yangkangkai <452306233@qq.com>
2018-11-20 17:08:10 +0800

    课程表改进

---
+++ courses_info.js
@@ -0,0 +1,31 @@
+		//测试数据集，课程固定，静态，不需要服务器支持。Time是将时间转为分钟，比如10小时变为600分钟。
+		var fjl = [
+			[{'cousrName':'尊巴Zumba', 'TrainName':'Darren', 'Time':'600' ,'duration':'50'},
+			 {'cousrName':'Aerobic Exercise综合有氧', 'TrainName':'Darren', 'Time':'1110' ,'duration':'50'},
+			 {'cousrName':'Yin yoga阴瑜伽', 'TrainName':'Yiyi', 'Time':'1170' ,'duration':'50'},
+			 {'cousrName':'Ready to go 新手入门', 'TrainName':'Darren', 'Time':'1170' ,'duration':'50'}],
+			
+			[{'cousrName':'Yoga physical 理疗瑜伽', 'TrainName':'Yiyi', 'Time':'840' ,'duration':'50'},
+			 {'cousrName':'Flow 流瑜伽', 'TrainName':'Darren', 'Time':'1170' ,'duration':'50'}],
+			
+			[{'cousrName':'Flow yoga 流瑜伽', 'TrainName':'Yiyi', 'Time':'600' ,'duration':'50'},
+			 {'cousrName':'Fight FX 能量搏击', 'TrainName':'Darren', 'Time':'1110' ,'duration':'50'},
+			 {'cousrName':'Yin yoga阴瑜伽', 'TrainName':'Yiyi', 'Time':'1170' ,'duration':'50'},
+			 {'cousrName':'Training Day 训练日', 'TrainName':'Darren', 'Time':'1170' ,'duration':'50'}],
+			
+			[{'cousrName':'Hatha Yogaha哈他瑜伽', 'TrainName':'Yiyi', 'Time':'600' ,'duration':'50'},
+			 {'cousrName':'Zumba 尊巴', 'TrainName':'Darren', 'Time':'1110' ,'duration':'50'},
+			 {'cousrName':'Hatha yoga 哈他瑜伽', 'TrainName':'Yiyi', 'Time':'1170' ,'duration':'50'},
+			 {'cousrName':'Ready to go 新手入门', 'TrainName':'Rambo', 'Time':'1170' ,'duration':'50'}],
+			
+			[{'cousrName':'Hatha Yoga 哈他瑜伽', 'TrainName':'Yiyi', 'Time':'840' ,'duration':'50'},
+			 {'cousrName':'Aerobic Exercise综合有氧', 'TrainName':'Darren', 'Time':'1110' ,'duration':'50'},
+			 {'cousrName':'Flow yoga 流瑜伽', 'TrainName':'Yiyi', 'Time':'1170' ,'duration':'50'},
+			 {'cousrName':'Schwinn', 'TrainName':'Darren', 'Time':'1170' ,'duration':'50'}],
+			
+			[{'cousrName':'Pump FX 功能杠铃', 'TrainName':'Darren', 'Time':'1110' ,'duration':'50'},
+			 {'cousrName':'Hatha Yoga 哈他瑜伽', 'TrainName':'Yiyi', 'Time':'1170' ,'duration':'50'},
+			 {'cousrName':'Ready to do 新手入门', 'TrainName':'Darren', 'Time':'1170' ,'duration':'50'}],
+			
+			[{'cousrName':'Fight FX 能量搏击', 'TrainName':'Darren', 'Time':'1110', 'duration':'50'}],
+		]
